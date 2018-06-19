3802e6af0ba9f6ae3acc40f3ecfbd886063c180b
Elan Fraiman <elan@elfraiman.com>
2018-06-19 16:25:05 +0200

    'added navigation'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Router, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatMenuModule,
+  MatIconModule,
+  MatToolbarModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+   user: object = {
+    name: 'Elan',
+    email: 'elfraiman@gmail.com'
+  }
+
+  userConnected: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
