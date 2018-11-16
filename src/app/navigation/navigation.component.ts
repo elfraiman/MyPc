76704a725c24940cf0674d0b84fd0ffe002f4f9b
elfraim <elfraiman@gmail.com>
2018-11-16 08:44:18 +0100

    feat(navbar): implemented new navigation bar and fixed some positioning in pages

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import * as firebase from 'firebase';
-import { Observable } from '../../../node_modules/rxjs';
+import { isNullOrUndefined } from 'util';
 import { filter, map } from '../../../node_modules/rxjs/operators';
 import { AuthService } from './../services/auth.service';
-import { isNullOrUndefined } from 'util';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
