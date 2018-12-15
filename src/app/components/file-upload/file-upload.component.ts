244390c262a1cebe493b04596007dd45fd5b1972
elfraim <elfraiman@gmail.com>
2018-12-15 13:26:38 +0100

    fix(upload): more progress on upload also added per user storage in firebase

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { isNullOrUndefined } from 'util';
+import { filter, map, take } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -25,7 +28,12 @@ export class FileUploadComponent {
   // State for dropzone CSS toggling
   isHovering: boolean;
 
-  constructor(private storage: AngularFireStorage, private db: AngularFirestore) { }
+  public userEmail$ = this.afAuth.authState.pipe(
+    filter(val => !isNullOrUndefined(val)),
+    map(val => val.email)
+  );
+
+  constructor(private storage: AngularFireStorage, private db: AngularFirestore, private afAuth: AngularFireAuth) { }
 
 
   toggleHover(event: boolean) {
@@ -37,14 +45,16 @@ export class FileUploadComponent {
     // The File object
     const file = event.item(0);
 
+    const userEmail = await this.userEmail$.pipe(take(1)).toPromise();
+
     // Client-side validation example
-    if (file.type.split('/')[0] !== 'image') {
+   /*  if (file.type.split('/')[0] !== 'image') {
       console.error('unsupported file type :( ');
       return;
-    }
+    } */
 
     // The storage path
-    const path = `test/${new Date().getTime()}_${file.name}`;
+    const path = `user-uploads/${userEmail}/${new Date().getTime()}_${file.name}`;
 
     // Totally optional metadata
     const customMetadata = { app: 'My AngularFire-powered PWA!' };
